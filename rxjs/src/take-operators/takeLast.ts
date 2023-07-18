d1033ca4be0a48285f050378e75eaf64f7c02e76
Jaeha Lee <wogkdkrm112@gmail.com>
2023-07-18 16:25:03 +0900

    feat: takeLast

--- rxjs/src/take-operators/takeLast.ts
+++ rxjs/src/take-operators/takeLast.ts
@@ -13,8 +13,36 @@ range(1, 20).pipe(takeLast(5)).subscribe(console.log);
 
 console.log('------------');
 
+// 끝이 언제인지 모기 때문에 출력 안됨
 interval(1000)
   .pipe(takeLast(5))
+  .subscribe(
+    console.log,
+    (err: Error) => console.error(err),
+    () => console.log('COMPLETE')
+  );
+
+// 끝이 언제인지 알고 출력됨
+interval(1000)
+  .pipe(take(10), takeLast(5))
+  .subscribe(
+    console.log,
+    (err: Error) => console.error(err),
+    () => console.log('COMPLETE')
+  );
+
+// 잘못된 방법
+fromEvent(document, 'click')
+  .pipe(takeLast(5), pluck('x'))
+  .subscribe(
+    console.log,
+    (err: Error) => console.error(err),
+    () => console.log('COMPLETE')
+  );
+
+//
+fromEvent(document, 'click')
+  .pipe(take(10), takeLast(5), pluck('x'))
   .subscribe(
     console.log,
     (err) => console.error(err),
